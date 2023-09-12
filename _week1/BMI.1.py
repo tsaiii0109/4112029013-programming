7bf803bf0aa7def92c743621094cf760e151548f
tsaiii0109 <s112029013@smail.nchu.edu.tw>
2023-09-12 20:57:26 +0800

    s

---
+++ _week1/BMI.1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Height=float(input("請輸入您的身高(公尺):"))
+Weight=float(input("請輸入您的體重(公斤):"))
+BMI=Weight/(Height**2)
+print(f"您的BMI值為:{BMI:.2f}")
+
+
+# In[ ]:
+
+
+
+
---
+++ _week1/BMI.2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Height=float(input("請輸入您的身高(公尺):"))
+Weight=float(input("請輸入您的體重(公斤):"))
+BMI=Weight/(Height**2)
+print(f"您的BMI值為:{BMI:.2f}")
